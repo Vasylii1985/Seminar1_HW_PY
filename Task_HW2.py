5820c48df876a81bdad95acc5f5f8b189b31ca1f
Vasilii <bespalchuk1985@gmail.com>
2022-07-14 14:38:15 +0300

    Task2

---
+++ Task_HW2.py
@@ -0,0 +1,10 @@
+day = int(input('Введите номер дня: '))
+def DeyOfTheWeek(a):
+    if day > 7 or day < 1:
+        print('Повторите ввод')
+    elif day == 6 or day == 7:
+        print("Да, это выходной!")
+    else:
+        print("Нет, это не выходной!")
+
+DeyOfTheWeek(day)
